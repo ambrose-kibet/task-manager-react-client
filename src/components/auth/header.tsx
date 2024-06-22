824225090bde161c694bf434ec657f75f958f18c
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-22 21:45:58 +0300

    feat: Add Header component for displaying the header in the authentication flow

---
+++ src/components/auth/header.tsx
@@ -0,0 +1,15 @@
+import Logo from "../logo";
+
+const Header: React.FC<{ label: string }> = ({ label }) => {
+  return (
+    <div className={"w-full space-y-4"}>
+      <div className="flex items-center justify-center">
+        <Logo />
+      </div>
+      <p className="text-center text-sm capitalize text-muted-foreground">
+        {label}
+      </p>
+    </div>
+  );
+};
+export default Header;
