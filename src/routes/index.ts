ba75c0b03b702dda12ee307e389407032d79224c
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-22 21:48:45 +0300

    feat: Add landing page, my tasks page, and stats page components

---
+++ src/routes/index.ts
@@ -0,0 +1,34 @@
+import AdminPage, { loader as adminLoader } from "./admin-page";
+import AdminRoutesLayout from "./admin-routes-layout";
+import AuthPage from "./auth-page";
+import AuthRoutesLayout from "./auth-routes-layout";
+import CreateTasksPage from "./create-tasks-page";
+import ErrorPage from "./error-page";
+import ForgotPasswordPage from "./forgot-password-page";
+import LandingPage from "./landing-page";
+import MyTasksPage from "./my-tasks-page";
+import ProfilePage from "./profile-page";
+import ProtectedRoutesLayout from "./protected-routes-layout";
+import ResetPasswordPage from "./reset-password-page";
+import Root from "./root";
+import StatsPage from "./stats-page";
+import VerifyEmailPage from "./verify-email-page";
+
+export {
+  ErrorPage,
+  AuthPage,
+  AuthRoutesLayout,
+  LandingPage,
+  MyTasksPage,
+  ProtectedRoutesLayout,
+  Root,
+  CreateTasksPage,
+  ProfilePage,
+  ForgotPasswordPage,
+  ResetPasswordPage,
+  AdminPage,
+  AdminRoutesLayout,
+  StatsPage,
+  VerifyEmailPage,
+  adminLoader,
+};
