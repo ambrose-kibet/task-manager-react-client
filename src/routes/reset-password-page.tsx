eb65bf797b5177329970d07f0d86fc21ac7eec21
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-22 21:49:18 +0300

    feat: Add ResetPasswordPage component for handling password reset

---
+++ src/routes/reset-password-page.tsx
@@ -0,0 +1,10 @@
+import ResetPasswordForm from "@/components/auth/reset-password";
+
+const ResetPassword: React.FC = () => {
+  return (
+    <div className="min-w-screen flex min-h-screen flex-col items-center justify-center px-2">
+      <ResetPasswordForm />
+    </div>
+  );
+};
+export default ResetPassword;
