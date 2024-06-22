7d4cf8ff45ddd7670963190473574241fcd1e7e6
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-22 21:48:37 +0300

    feat: Add ForgotPasswordPage component for handling password recovery

---
+++ src/routes/forgot-password-page.tsx
@@ -0,0 +1,10 @@
+import ForgotPassword from "@/components/auth/forgot-password";
+
+const ForgotPasswordPage: React.FC = () => {
+  return (
+    <div className="min-w-screen flex min-h-screen flex-col items-center justify-center px-2">
+      <ForgotPassword />
+    </div>
+  );
+};
+export default ForgotPasswordPage;
