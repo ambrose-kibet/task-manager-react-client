db2e50b1351934ab8bd33991d8239fc7d116e68d
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-24 20:30:26 +0300

    feat: Update CreateTasksPage to include CreateTaskForm and TasksByDay components
    This commit updates the CreateTasksPage component to include the CreateTaskForm and TasksByDay components. The CreateTaskForm component allows users to create new tasks, while the TasksByDay component displays tasks for the current day. This enhancement improves the functionality and user experience of the CreateTasksPage.

--- src/routes/create-tasks-page.tsx
+++ src/routes/create-tasks-page.tsx
@@ -1,4 +1,13 @@
+import CreateTaskForm from "@/components/tasks/create-task";
+import TasksByDay from "@/components/tasks/tasks-by-day";
+import moment from "moment";
+
 const CreateTasksPage: React.FC = () => {
-  return <div>create-tasks-page</div>;
+  return (
+    <div className="min-w-screen flex min-h-screen flex-col items-center justify-center gap-y-4 p-2">
+      <CreateTaskForm />
+      <TasksByDay date={moment(new Date()).format("YYYY-MM-DD")} />
+    </div>
+  );
 };
 export default CreateTasksPage;
