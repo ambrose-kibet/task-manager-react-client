cbcb22ab2cb57dbcb079ea2d055d5353cd508adf
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-24 20:28:49 +0300

    feat: Add Linechart component for displaying line charts

---
+++ src/components/charts/linechart.tsx
@@ -0,0 +1,29 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const Linechart: React.FC<{
+  data: { date: string; count: number; verified: number }[];
+  xKey: string;
+  yKey: string;
+}> = ({ data = [], xKey = "", yKey = "" }) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart data={data}>
+        <XAxis dataKey={xKey} />
+        <YAxis label={{ value: yKey, angle: -90, position: "insideLeft" }} />
+        <Tooltip />
+        <Legend />
+        <Line dataKey="completed" type="monotone" stroke="#16a34a" />
+        <Line dataKey="created" type={"monotone"} stroke="#8884d8" />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
+export default Linechart;
