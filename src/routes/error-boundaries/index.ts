1165cad7a751717705035f929140257fcf19b2e7
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-24 20:30:55 +0300

    feat: Add TaskPerDayErrorBoundary component for handling errors in tasks per day page

--- src/routes/error-boundaries/index.ts
+++ src/routes/error-boundaries/index.ts
@@ -1,4 +1,5 @@
 import AuthErrorBoundary from "./auth-error-boundary";
 import AdminErrorBoundary from "./admin-error-boundary";
+import TaskPerDayErrorBoundary from "./tasks-per-day error-boundary";
 
-export { AuthErrorBoundary, AdminErrorBoundary };
+export { AuthErrorBoundary, AdminErrorBoundary, TaskPerDayErrorBoundary };
