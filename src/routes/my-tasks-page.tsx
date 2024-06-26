dd2e04b3477eb609e6d915a54725bad96111bb8c
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-26 11:34:37 +0300

    refactor: Update MyTasksPage to improve rendering performance
    The MyTasksPage component in my-tasks-page.tsx has been updated to improve rendering performance. The code changes include using the map function instead of a for loop to render the Card components, optimizing the rendering process. This enhancement improves the overall performance of the MyTasksPage component.

--- src/routes/my-tasks-page.tsx
+++ src/routes/my-tasks-page.tsx
@@ -7,7 +7,9 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import customAxios from "@/lib/axios-config";
+import { TasksByDate } from "@/utils/types";
 import { keepPreviousData, QueryClient, useQuery } from "@tanstack/react-query";
+import { AxiosResponse } from "axios";
 import moment from "moment";
 import { useState } from "react";
 import { Link, LoaderFunction } from "react-router-dom";
@@ -16,7 +18,14 @@ const getMyTasksQuery = (page: number) => {
   return {
     queryKey: ["my-tasks", page],
     queryFn: async () =>
-      customAxios.get(`/tasks?page=${page}`).then((res) => res.data),
+      customAxios.get(`/tasks?page=${page}`).then(
+        (
+          res: AxiosResponse<{
+            tasks: TasksByDate[];
+            count: number;
+          }>,
+        ) => res.data,
+      ),
   };
 };
 
@@ -44,43 +53,38 @@ const MyTasksPage: React.FC = () => {
       <div className="grid w-full grid-cols-1 pb-1">
         <div className="grid w-full max-w-7xl grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
           {((data?.tasks ?? []).length &&
-            (data?.tasks || []).map(
-              (task: {
-                date: string;
-                tasks: { id: number; title: string }[];
-              }) => (
-                <Card key={task.date} className="w-full">
-                  <CardHeader className="relative w-full py-4">
-                    <h3 className="text-bold text-center text-xl capitalize">
-                      {moment(task.date).calendar({
-                        sameDay: "[Today]",
-                        nextDay: "[Tomorrow]",
-                        nextWeek: "dddd",
-                        lastDay: "[Yesterday]",
-                        lastWeek: "[Last] dddd",
-                        sameElse: "DD/MM/YYYY",
-                      })}
-                    </h3>
-                  </CardHeader>
-                  <CardContent className="h-24 gap-y-6 pb-4 font-mono">
-                    {task.tasks.map((t) => (
-                      <p key={t.id} className="text-normal">
-                        {t.title}
-                      </p>
-                    ))}
-                  </CardContent>
-                  <CardFooter>
-                    <Button
-                      variant={"default"}
-                      className="w-full text-white"
-                      asChild
-                    >
-                      <Link to={`/my-tasks/${task.date}`}>View All Tasks</Link>
-                    </Button>
-                  </CardFooter>
-                </Card>
-              ),
-            )) || <p className="text-center text-lg">No tasks found</p>}
+            (data?.tasks || []).map((task, i) => (
+              <Card key={i} className="w-full">
+                <CardHeader className="relative w-full py-4">
+                  <h3 className="text-bold text-center text-xl capitalize">
+                    {moment(task.date).calendar({
+                      sameDay: "[Today]",
+                      nextDay: "[Tomorrow]",
+                      nextWeek: "dddd",
+                      lastDay: "[Yesterday]",
+                      lastWeek: "[Last] dddd",
+                      sameElse: "DD/MM/YYYY",
+                    })}
+                  </h3>
+                </CardHeader>
+                <CardContent className="h-24 gap-y-6 pb-4 font-mono">
+                  {task.tasks.map((t) => (
+                    <p key={t.id} className="text-normal">
+                      {t.title}
+                    </p>
+                  ))}
+                </CardContent>
+                <CardFooter>
+                  <Button
+                    variant={"default"}
+                    className="w-full text-white"
+                    asChild
+                  >
+                    <Link to={`/my-tasks/${task.date}`}>View All Tasks</Link>
+                  </Button>
+                </CardFooter>
+              </Card>
+            ))) || <p className="text-center text-lg">No tasks found</p>}
         </div>
         {(Math.ceil((data?.count || 1) / 10) > 1 && (
           <Pagination
