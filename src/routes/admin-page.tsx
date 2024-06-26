ce12aaa4997f4f3c9f806de82e2b318a6c0761a5
ambrose-kibet <ambrosekibet36@gmail.com>
2024-06-26 11:34:02 +0300

    refactor: Improve error handling in admin-page.tsx
    The admin-page.tsx file has been refactored to improve error handling. The code changes include updating the error handling logic in the getSignUpStatsQuery and getAllUsersQuery functions. The error handling now correctly throws an error with the appropriate error message when an error occurs during the API request. This refactor enhances the reliability and user experience of the admin page.

--- src/routes/admin-page.tsx
+++ src/routes/admin-page.tsx
@@ -2,15 +2,31 @@ import UserStats from "@/components/admin/user-stats";
 import Users, { filterSortTypes } from "@/components/admin/users";
 import customAxios from "@/lib/axios-config";
 import { QueryClient } from "@tanstack/react-query";
+import { AxiosError, AxiosResponse } from "axios";
 
 export const getSignUpStatsQuery = (
   duration: "daily" | "weekly" | "monthly",
 ) => ({
   queryKey: ["sign-up-stats", duration],
   queryFn: () =>
-    customAxios
-      .get(`/users/signup-stats?duration=${duration}`)
-      .then((res) => res.data),
+    customAxios.get(`/users/signup-stats?duration=${duration}`).then(
+      (
+        res: AxiosResponse<
+          Array<{
+            date: string;
+            verified: number;
+            count: number;
+          }>
+        >,
+      ) => res.data,
+    ),
+
+  onError: (error: Error) => {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data.message || "An error occurred");
+    }
+    throw new Error(error.message || "An error occurred");
+  },
 });
 
 export const getAllUsersQuery = ({ page, email, sort }: filterSortTypes) => ({
@@ -20,7 +36,26 @@ export const getAllUsersQuery = ({ page, email, sort }: filterSortTypes) => ({
       .get(
         `/users?page=${page}&sortby=${sort}${email ? `&email=${email}` : ""}`,
       )
-      .then((res) => res.data),
+      .then(
+        (
+          res: AxiosResponse<{
+            users: Array<{
+              id: string;
+              email: string;
+              name: string;
+              avatar: string | null;
+              role: "USER" | "ADMIN";
+            }>;
+            count: number;
+          }>,
+        ) => res.data,
+      ),
+  onError: (error: Error) => {
+    if (error instanceof AxiosError) {
+      throw new Error(error.response?.data.message || "An error occurred");
+    }
+    throw new Error(error.message || "An error occurred");
+  },
 });
 
 export const loader = (queryClient: QueryClient) => () => {
