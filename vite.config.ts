79d742e48bed64eb9fc311ec4098e703c28994a7
ambrose-kibet <ambrosekibet36@gmail.com>
2024-07-11 20:31:37 +0300

    refactor: Update vitest dependency to version 2.0.1
    The vitest dependency in package.json has been updated to version 2.0.1. This update ensures compatibility with the latest testing features and improvements.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +9,10 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/tests/setupTests.ts",
+    globals: true,
   },
 });
