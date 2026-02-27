40f75a4ed81a0cc0dd48d120de65f4d80767a40f
ambrose-kibet <ambrosekibet36@gmail.com>
2026-02-27 17:00:39 +0300

    fix: Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  base: "/task-manager-react-client/",
   plugins: [react()],
   resolve: {
     alias: {
